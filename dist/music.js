72fff9eba7b94502507bea2803bb746d537bbbb4
987620608@qq.com <987620608@qq.com>
2024-11-01 22:49:08 +0800

    Site updated: 2024-11-01 22:48:58

--- dist/music.js
+++ dist/music.js
@@ -7,10 +7,10 @@ const ap = new APlayer({
     mini: true,
     audio: [ //音频信息,包含以下
         {
-            name: 'Happy(Prod. Hedyy)', //音频名称
-            artist: '차노을', //音频艺术家
-            url: 'http://music.163.com/song/media/outer/url?id=2151605096.mp3', //音频外链
-            cover: 'https://y.qq.com/music/photo_new/T002R300x300M000004B0tMb4WO8Yz_3.jpg?max_age=2592000', //音频封面
+            name: '山脚', //音频名称
+            artist: 'Jony J', //音频艺术家
+            url: 'http://music.163.com/song/media/outer/url?id=1886366521.mp3', //音频外链
+            cover: 'http://p1.music.126.net/JMdgOVaMn2myYZy1TIKPNA==/109951169213952419.jpg?param=130y130', //音频封面
             lrc: 'lrc1.lrc', //音频歌词，配合上面的lrcType使用
             theme: '#ffffff' //切换到此音频时的主题色，比上面的 theme 优先级高
         },
